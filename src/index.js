6cb61c59b643f8fb4d444b79fd6ed0663c004bbe
Rohan Sharma <70686943+ronnygaur@users.noreply.github.com>
2023-05-10 12:28:24 +0530

    Desktop

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { ChakraBaseProvider, ColorModeScript , theme  } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import ColorModeSwitcher from './ColorModeSwitcher'
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <ColorModeScript />
+
+
+    <ChakraBaseProvider theme={theme}>
+
+      <ColorModeSwitcher />
+    <App />      
+    </ChakraBaseProvider>
+    
+  </StrictMode>
+);
+
+
